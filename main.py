c2cd39419246e04a41eb79134bd78234d31baceb
HeavenManySugar <chy853@gmail.com>
2022-06-06 14:33:34 +0800

    Update main.py
    slash command
    using pycord

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import discord, json, random, os
-from discord.ext import commands
+import discord, json, os
 from dotenv import load_dotenv
+from discord.ext import commands
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
@@ -8,26 +8,43 @@ TOKEN = os.getenv("TOKEN")
 with open('setting.json', 'r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
 
-bot = commands.Bot(command_prefix=jdata['Prefix'])
+bot = commands.Bot(command_prefix=commands.when_mentioned_or(jdata['Prefix']))
 
 @bot.event
 async def on_ready():
-    print(">> Bot is online <<")
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"Music | /play"))
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("------")
+
+@bot.slash_command()
+async def hello(ctx):
+    await ctx.respond("Hello!")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
 
 @bot.command()
 async def load(ctx, extension):
     bot.load_extension(f'cmds.{extension}')
     await ctx.send(f'```Loaded {extension} done.```')
+    
+@bot.slash_command()
+async def load(ctx, extension):
+    bot.load_extension(f'cmds.{extension}')
+    await ctx.respond(f'```Loaded {extension} done.```')
 
-@bot.command()
+#@bot.command()
+@bot.slash_command()
 async def unload(ctx, extension):
     bot.unload_extension(f'cmds.{extension}')
-    await ctx.send(f'```Un - Loaded {extension} done.```')
+    await ctx.respond(f'```Un - Loaded {extension} done.```')
 
-@bot.command()
+#@bot.command()
+@bot.slash_command()
 async def reload(ctx, extension):
     bot.reload_extension(f'cmds.{extension}')
-    await ctx.send(f'```Re - Loaded {extension} done.```')
+    await ctx.respond(f'```Re - Loaded {extension} done.```')
 
 for filename in os.listdir('./cmds'):
     if filename.endswith('.py'):
